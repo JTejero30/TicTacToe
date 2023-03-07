a1cbbaa3d538652c0d9eb5fa87cac1bfa45fb576
Javi Tejero <javitejero@93gmail.com>
2023-03-07 20:28:05 +0100

    Estilo cambiado a pizarra, Conseguido pintar, planteado marcador, planteado no pintar mas

--- app.js
+++ app.js
@@ -1,27 +1,92 @@
-let main, casilla, casillaLong
+let main, casilla, casillaLong, numero, turno, marcador, jugadorX, ganadasX, ganadasO
 
-main=document.getElementsByTagName('main')[0];
-casilla=document.getElementsByClassName('casilla');
-console.log(casilla);
-casillaLong=document.getElementsByClassName('casilla').length;
+main = document.getElementsByTagName('main')[0];
+casilla = document.getElementsByClassName('casilla');
 
-function asignarAtributo() {
-    for (let i = 0; i < casillaLong; i++) {
-        console.log(casilla[i]);
+
+for (let i = 0; i < casilla.length; i++) {
+    casilla[i].setAttribute('onclick', `ponerFicha(${i})`);
+
+}
+
+
+
+/*funcion ponerFicha
+@param numero de casilla
+*/
+turno = true
+function ponerFicha(numero) {
+
+    do{
+    if (turno == true) {
+        casilla[numero].textContent = 'X'
+        ganar('X');
+    } else {
+        casilla[numero].textContent = 'O'
+        ganar('O');
+    }
+    casilla[numero].removeAttribute('onclick')
+    
+    turno = !turno;
+}while (!ganar) {
+    
+}/*no funciona*/ 
+}
+
+/*ganar()
+@param jugador que es tratado como string X u O y asi comparar fichas de uno y de otro en la misma funcion */ 
+
+function ganar(jugador) {
+    let victoria= false
+    let ganadoras = [
+        //filas
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        //columnas
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        //diagonales
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    let fichas = []
+
+    for (let i = 0; i < casilla.length; i++) {
+        if (casilla[i].textContent == jugador) {
+            fichas.push(i)
+        }
+    }
+    for (let j = 0; j < ganadoras.length; j++) {
+        if (fichas.includes(ganadoras[j][0])&&(fichas.includes(ganadoras[j][1]))&&(fichas.includes(ganadoras[j][2]))) {
+            victoria=true
+            alert('ganan')
+        }
         
     }
+    return victoria
 }
 
-// for(c in casilla){
+/*****************MARCADOR*******************/
+marcador=document.getElementsByClassName('marcador')[0]
+jugadorX=document.createElement('div')
+jugadorX.setAttribute('class','jugadorX')
+jugadorX.textContent=`Jugador X: ${ganadasX}`
+marcador.appendChild(jugadorX)
+jugadorO=document.createElement('div')
+jugadorO.setAttribute('class','jugadorO')
+jugadorO.textContent=`Jugador O: ${ganadasO}`
+marcador.appendChild(jugadorO) 
 
-//     casilla[c].textContent='X'
-// }
-
-
-
-function ponerFicha() {
-    for(c in casilla){
-        casilla[c].textContent="X"
-    }
+function cambiarMarcador() {
+    
 }
+
+
+
+
+
+
+
 
